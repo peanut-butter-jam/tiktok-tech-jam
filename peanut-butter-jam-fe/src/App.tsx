81961e39ab3fabaa937bf3b1e1e09d4a5f708249
TehOPanas <e0960514@u.nus.edu.sg>
2025-08-27 15:14:21 +0800

    setup routing and pages

--- peanut-butter-jam-fe/src/App.tsx
+++ peanut-butter-jam-fe/src/App.tsx
@@ -1,55 +1,15 @@
-import { useCallback, useEffect, useState } from '@lynx-js/react'
+import { MemoryRouter, Route, Routes } from 'react-router';
 
-import './App.css'
-import arrow from './assets/arrow.png'
-import lynxLogo from './assets/lynx-logo.png'
-import reactLynxLogo from './assets/react-logo.png'
+import './App.css';
 
-export function App(props: {
-  onRender?: () => void
-}) {
-  const [alterLogo, setAlterLogo] = useState(false)
-
-  useEffect(() => {
-    console.info('Hello, ReactLynx')
-  }, [])
-  props.onRender?.()
-
-  const onTap = useCallback(() => {
-    'background only'
-    setAlterLogo(prevAlterLogo => !prevAlterLogo)
-  }, [])
-
+export function App() {
   return (
-    <view>
-      <view className='Background' />
-      <view className='App'>
-        <view className='Banner'>
-          <view className='Logo' bindtap={onTap}>
-            {alterLogo
-              ? <image src={reactLynxLogo} className='Logo--react' />
-              : <image src={lynxLogo} className='Logo--lynx' />}
-          </view>
-          <text className='Title'>React</text>
-          <text className='Subtitle'>on Lynx</text>
-        </view>
-        <view className='Content'>
-          <image src={arrow} className='Arrow' />
-          <text className='Description'>Tap the logo and have fun!</text>
-          <text className='Hint'>
-            Edit<text
-              style={{
-                fontStyle: 'italic',
-                color: 'rgba(255, 255, 255, 0.85)',
-              }}
-            >
-              {' src/App.tsx '}
-            </text>
-            to see updates!
-          </text>
-        </view>
-        <view style={{ flex: 1 }} />
-      </view>
-    </view>
-  )
+    <MemoryRouter>
+      <Routes>
+        <Route path="/" element={<view></view>} />
+        <Route path="/regulations" element={<view></view>} />
+        <Route path="/features" element={<view></view>} />
+      </Routes>
+    </MemoryRouter>
+  );
 }
