2a271a31fe0b32ffbf6c2ce6ba6f195f3c6cefd7
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2025-08-29 14:29:55 +0800

    Add script to autogen api types

---
+++ frontend/src/types/dto.ts
@@ -0,0 +1,12 @@
+import { type components } from "./api";
+
+// Regulation Endpoints
+export type RegulationDTO = components["schemas"]["RegulationDTO"];
+export type RegulationCreateDTO = components["schemas"]["RegulationCreateDTO"];
+export type RouDTO = components["schemas"]["RouDto"];
+
+// Feature Endpoints
+export type FeatureDTOWithCheck = components["schemas"]["FeatureDTOWithCheck"];
+export type FeatureCreateDTO = components["schemas"]["FeatureCreateDTO"];
+export type CheckDTO = components["schemas"]["CheckDTO"];
+export type EvalResultDTO = components["schemas"]["EvalResultDTO"];
