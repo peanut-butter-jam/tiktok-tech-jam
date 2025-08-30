f919bc05d921f313ae2fa61bcb6f9ac874509136
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2025-08-30 22:16:26 +0800

    Limit max concurrent evaluations

--- api/app/services/feature/feat_eval/feature_eval_service.py
+++ api/app/services/feature/feat_eval/feature_eval_service.py
@@ -10,6 +10,9 @@ from app.services.feature.feature_service import FeatureServiceDep
 from app.services.feature.feat_eval.feat_eval_agent import FeatEvalAgentDep
 from app.services.feature.feat_eval.term_mapping_agent import term_mapping_agent_context
 
+# Max 10 concurrent evaluations
+semaphore = asyncio.Semaphore(15)
+
 
 class FeatureEvalService:
     def __init__(
@@ -25,13 +28,14 @@ class FeatureEvalService:
         self.background_tasks = background_tasks
 
     async def _trigger_eval(self, feature: FeatureDTO, check: CheckDTO):
-        feature_to_eval = feature
+        async with semaphore:
+            feature_to_eval = feature
 
-        if not feature.terminologies:
-            async with term_mapping_agent_context() as term_mapping_agent:
-                feature_to_eval = await term_mapping_agent.ainvoke(feature)
+            if not feature.terminologies:
+                async with term_mapping_agent_context() as term_mapping_agent:
+                    feature_to_eval = await term_mapping_agent.ainvoke(feature)
 
-        await self.feat_eval_agent.ainvoke(feature_to_eval, check)
+            await self.feat_eval_agent.ainvoke(feature_to_eval, check)
 
     async def trigger_evals(self, feature_ids: List[int]) -> List[CheckDTO]:
         checks = await self.check_service.init_checks(feature_ids, check_type=CheckType.AI)
